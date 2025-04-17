16080adfaa73dc8e6c63c6a51404f989186a8165
Olivia Sharpston <osharpston@una.edu>
2025-04-17 11:40:13 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -3,6 +3,3 @@ Last Project for CIS376
 
 Modal Reference: https://www.w3schools.com/bootstrap5/bootstrap_modal.php
 Text Area Reference: https://www.w3schools.com/tags/tag_textarea.asp
-Change Login Buttone Color Reference: https://getbootstrap.com/docs/4.0/components/buttons/
-
-Look into Copilot
---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    //code goes here
+  
+    $("#saveBook").on("click", function () {
+      //call f/n
+      let statusText = underConstruction("Not Ready, Please be Patient!"); //update the status
+      $("#saveBook").text(statusText).prop("disabled", true);
+    
+    });
+  
+    //pass a status (ready or not ready) and get back a message for user
+    //rn, not ready
+    function underConstruction(status) {
+      return "Not Ready, Sorry!";
+    }
+
+    let name = "";
+    if(username){
+        name = username;
+        //(text) = Welcome, ${name};
+        // text: welcome username
+        $("logIn").hide();
+        $("logOut").show();
+    }
+  });
+  
